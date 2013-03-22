1c00c5b5aec4848e77e40b75290e30fd5c911b1f
urbanczykd <urbanczykd@gmail.com>
2013-03-22 11:33:25 +0100

    initialize app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Groupblog::Application.config.secret_token = 'd10846b537c3456f9beb45fe4941b9e2b7f7a8b2a5f9b50e088f2f5a422b23a070aefc177fed051049b8c56b08765b03900db9257f5d5fdae06c2898e7018392'
